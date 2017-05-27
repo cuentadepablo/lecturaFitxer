0ca9ad6f83d43e05401d27202c4db95cafcd15ff
mrpotes <mrpotes@192.168.1.8>
2017-05-27 09:55:34 +0200

    desde el mac

---
+++ src/lecturadefichero/LecturaDeFichero.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lecturadefichero;
+import java.io.File;
+import java.util.Scanner;
+/**
+ *
+ * @author mrpotes
+ */
+public class LecturaDeFichero {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // fichero que queremos leer
+        File fichero  =  new File ("/Users/mrpotes/tmp.txt");
+        Scanner s = null;
+        try {
+            //leemos el contenido del fichero
+            System.out.println("Leyendo el fichero ..............");
+            s= new Scanner (fichero);
+            //leemos linea a linea el fichero
+            while (s.hasNextLine()){
+                String linea =s.nextLine();
+                System.out.println(linea);
+                
+            }
+        }catch(Exception Ex){
+            System.out.println("Mensaje: "+ Ex.getMessage());
+        } finally {
+            // Cerramos el fichero tanto si hemos leido o no el fichero
+            try{
+                if(s!=null){
+                  s.close();
+                }
+            }catch(Exception Ex2){
+                    System.out.println("Mensaje de Error2: "+ Ex2.getMessage());
+                          
+            }
+        }
+    }
+}
+    
+
